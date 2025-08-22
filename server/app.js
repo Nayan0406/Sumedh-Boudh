5b18ed1fca7fd8c3032b1f620de6a5fcf021fe91
Nayan0406git config --global user.email <nayannikhare83@gmail.com>
2025-08-22 12:57:26 +0530

    local url add

--- server/app.js
+++ server/app.js
@@ -23,7 +23,11 @@ app.use(express.json());
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ["http://127.0.0.1:5500/", "https://sumedh-boudh-backend.vercel.app/"],
+  origin: [
+    "http://127.0.0.1:5500",
+    "http://localhost:5000", 
+    // "https://your-vercel-domain.vercel.app"
+  ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   credentials: true,
 }));
